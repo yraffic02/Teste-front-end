4b93c48fac7517792f5b6bd2bf194304bee39ae1
yraffic02 <yraffic02@gmail.com>
2024-02-05 17:16:05 -0300

    chore: adicionado thema deafaul e global style ao escopo do next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,14 @@
 import type { AppProps } from 'next/app'
+import { GlobalStyle } from '../styles/global'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle>
+        <Component {...pageProps} />
+      </GlobalStyle>
+    </ThemeProvider>
+  )
 }
