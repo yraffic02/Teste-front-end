22eadcd4e6d57ff462fc16950bc42efcb4e231c4
yraffic02 <yraffic02@gmail.com>
2024-02-06 11:36:19 -0300

    fix: correção de global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,14 @@ import type { AppProps } from 'next/app'
 import { GlobalStyle } from '../styles/global'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from '../styles/themes/default'
+import { Header } from '../components/Header'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle>
-        <Component {...pageProps} />
-      </GlobalStyle>
+      <GlobalStyle />
+      <Header />
+      <Component {...pageProps} />
     </ThemeProvider>
   )
 }
