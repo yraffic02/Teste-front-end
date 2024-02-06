0cc30e67ec8e455088009b98ba9d89da5939b62f
yraffic02 <yraffic02@gmail.com>
2024-02-06 14:13:08 -0300

    chore: adicionada a font inter

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="pt-BR">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
--- styles/themes/default.ts
+++ styles/themes/default.ts
@@ -9,32 +9,32 @@ export const defaultTheme = {
     graySecondary: "#747474"
   },
   fonts: {
-    fontH1: "700 2rem/2.5rem",
-    fontH2: "600 1.5rem/2rem",
-    fontH3: "700 1.25rem/1.5rem",
+    fontH1: "700 2rem/2.5rem 'Inter'",
+    fontH2: "600 1.5rem/2rem 'Inter'",
+    fontH3: "600 1.25rem/1.5rem 'Inter'",
     fontH4: "700 0.75rem/1rem",
 
-    body1Bold: "700 1rem/1.125rem",
-    body1Regular: "500 1rem/1.125rem",
-    body1Link: "400 1rem/1.125rem; text-decoration: underline",
+    body1Bold: "700 1rem/1.125rem 'Inter'",
+    body1Regular: "500 1rem/1.125rem 'Inter'",
+    body1Link: "400 1rem/1.125rem; 'Inter' text-decoration: underline",
 
-    body2Bold: "700 0.875rem/1rem ",
-    body2Regular: "400 0.875rem/1rem",
-    body2Link:  "400 0.875rem/1rem; text-decoration: underline",
+    body2Bold: "700 0.875rem/1rem 'Inter'",
+    body2Regular: "400 0.875rem/1rem 'Inter'",
+    body2Link:  "400 0.875rem/1rem; 'Inter' text-decoration: underline",
 
-    body3Bold: "700 0.75rem/1rem",
-    body3Regular: "400 0.75rem/1rem",
-    body3Link:  "400 0.75rem/1rem; text-decoration: underline",
+    body3Bold: "700 0.75rem/1rem 'Inter'",
+    body3Regular: "400 0.75rem/1rem 'Inter'",
+    body3Link:  "400 0.75rem/1rem; 'Inter'; text-decoration: underline",
 
     buttonSmall:
-      "600 0.875rem/1.5rem 'Nunito Sans', sans-serif; letter-spacing: 0.5px",
+      "600 0.875rem/1.5rem 'Inter'; letter-spacing: 0.5px",
     buttonMedium:
-      "600 1rem/1.5rem 'Nunito Sans', sans-serif; letter-spacing: 0.5px",
+      "600 1rem/1.5rem 'Inter'; letter-spacing: 0.5px",
     buttonLarge:
-      "600 1.125rem/1.5rem 'Nunito Sans', sans-serif; letter-spacing: 0.5px",
+      "600 1.125rem/1.5rem 'Inter'; letter-spacing: 0.5px",
 
-    label: "600 0.75rem/1rem 'Nunito Sans', sans-serif",
+    label: "600 0.75rem/1rem 'Inter'",
 
-    caption: "400 0.75rem/1rem 'Nunito Sans', sans-serif",
+    caption: "400 0.75rem/1rem 'Inter'",
   },
 } as const;
