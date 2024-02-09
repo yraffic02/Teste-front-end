5c22371a8d78c60b6e09d93927612f003efc0758
yraffic02 <yraffic02@gmail.com>
2024-02-09 16:55:27 -0300

    chore: aplicado hover no logo centro poekmon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,41 @@
 import Image from "next/image"
 import { HeaderContainer, HeaderDiv, HeaderLinkPrimary, HeaderLinkSecondary, HeaderLogo, HeaderLogoTitle } from "./style"
 import Link from "next/link"
+import { useEffect, useState } from "react";
 
 export const Header = () =>{
+    const [isLogoOpen, setIsLogoOpen] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLogoOpen(false);
+        }, 5000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
+    const handleMouseEnter = () => {
+        setIsLogoOpen(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsLogoOpen(false);
+    };
+    
     return (
         <HeaderContainer>
-            <HeaderLogo>
+            <HeaderLogo
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                isOpen={isLogoOpen}
+            >
                 <Image
                     src='/images/white-pokeball.svg'
                     height={37}
                     width={37}
                     alt="logo poke"
                 />
-                <HeaderLogoTitle>
+                <HeaderLogoTitle isOpen={isLogoOpen}>
                     Centro Pokémon
                 </HeaderLogoTitle>
             </HeaderLogo>
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,6 +1,10 @@
 import Link from "next/link";
 import styled from "styled-components";
 
+interface isOpen {
+    isOpen: boolean
+}
+
 export const HeaderContainer = styled.header`
     width: 100vw;
     height: 6.5rem;
@@ -13,26 +17,31 @@ export const HeaderContainer = styled.header`
     background-color: ${(props)=> props.theme.colors.white};
 `
 
-export const HeaderLogo = styled.div`
-    max-width: 16rem;
+export const HeaderLogo = styled.div<isOpen>`
     display: flex;
     align-items: center;
+    justify-content: ${(props)=> (props.isOpen ? 'center': '')};
 
     padding: 0.75rem;
 
     gap: 0.5rem;
 
-    border-radius: 3.125rem;
+    border-radius: ${(props)=> (props.isOpen ? ' 3.125rem': ' 50%')};
 
     background-color: ${(props)=> props.theme.colors.redPrimary};
 
     cursor: pointer;
+
+    overflow: hidden;
+    transition: width 0.5s ease;
+    width: ${(props) => (props.isOpen ? '16rem' : '4rem')};
 `
 
-export const HeaderLogoTitle = styled.h1`
+export const HeaderLogoTitle = styled.h1<isOpen>`
     font: ${(props)=> props.theme.fonts.fontH3};
     
     color: ${(props)=> props.theme.colors.white};
+    display: ${(props) => (props.isOpen ? 'block' : 'none')};
 `
 
 export const HeaderLinkPrimary = styled.p`
